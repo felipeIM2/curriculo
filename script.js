3333915c27ab809d0fe716f59167bbfb2e4e92c7
Luis Felipe Moreira <luisf.infu@gmail.com>
2025-05-05 23:25:33 -0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-
+//Tamanho maximo de Skills    
 let maxWidth = 240
 
+// Controle de tamanho de porcentagem de Skills
 let elements = {
-   e1: 8,
-   e2: 6,
-   e3: 9,
-   e4: 7,
-   e5: 6,
-   e6: 4,
-   e7: 6,
-   e8: 5,
-   e9: 6,
-   e10: 4,
+   e1: 7,
+   e2: 7,
+   e3: 8,
+   e4: 6,
+   e5: 4,
+   e6: 8,
+   e7: 5,
+   e8: 6,
+   e9: 4,
+   e10: 6,
 }
 
 let pixelValues = {}
@@ -21,22 +22,19 @@ Object.keys(elements).forEach(key => {
   pixelValues[key] = (percentage*10)/100 * maxWidth;
 });
  
-//console.log()
 
 let line = document.querySelectorAll(".line")
-
 line.forEach((e) => {
 
-  let p = e.querySelectorAll("p")
-
+  let p = e.querySelectorAll(".indicator")
+    
   if (p[0].id) {
-
     switch (p[0].id) {
       case "1":
         let i1 = e.querySelector(".indicator");
         if (i1) {
           setTimeout(() => {
-            i1.style.cssText = `border-bottom:rgb(224, 112, 8) solid 4px; width:${pixelValues.e1}px;`;
+            i1.style.cssText = `background-color: #FF6B35; width:${pixelValues.e1}px;`;
           }, 800);
         }
         break;
@@ -44,7 +42,7 @@ line.forEach((e) => {
         let i2 = e.querySelector(".indicator");
         if (i2) {
           setTimeout(() => {
-            i2.style.cssText = `border-bottom:rgb(53, 142, 202) solid 4px; width:${pixelValues.e2}px;`;
+            i2.style.cssText = `background-color: #3A86FF; width:${pixelValues.e2}px;`;
           }, 800);
         }
         break;
@@ -52,7 +50,7 @@ line.forEach((e) => {
         let i3 = e.querySelector(".indicator");
         if (i3) {
           setTimeout(() => {
-            i3.style.cssText = `border-bottom:rgb(241, 191, 24) solid 4px; width:${pixelValues.e3}px;`;
+            i3.style.cssText = `background-color: #FFD60A; width:${pixelValues.e3}px;`;
           }, 800);
         }
         break;
@@ -60,7 +58,7 @@ line.forEach((e) => {
         let i4 = e.querySelector(".indicator");
         if (i4) {
           setTimeout(() => {
-            i4.style.cssText = `border-bottom:#3178C6 solid 4px; width:${pixelValues.e4}px;`;
+            i4.style.cssText = `background-color: #4361EE; width:${pixelValues.e4}px;`;
           }, 800);
         }
         break;
@@ -68,7 +66,7 @@ line.forEach((e) => {
         let i5 = e.querySelector(".indicator");
         if (i5) {
           setTimeout(() => {
-            i5.style.cssText = `border-bottom:rgb(165, 165, 165) solid 4px; width:${pixelValues.e5}px;`;
+            i5.style.cssText = `background-color: #A0A0A0; width:${pixelValues.e5}px;`;
           }, 800);
         }
         break;
@@ -76,7 +74,7 @@ line.forEach((e) => {
         let i6 = e.querySelector(".indicator");
         if (i6) {
           setTimeout(() => {
-            i6.style.cssText = `border-bottom:rgb(90, 190, 212) solid 4px; width:${pixelValues.e6}px`;
+            i6.style.cssText = `background-color: #4CC9F0; width:${pixelValues.e6}px`;
           }, 800);
         }
         break;
@@ -84,7 +82,7 @@ line.forEach((e) => {
         let i7 = e.querySelector(".indicator");
         if (i7) {
           setTimeout(() => {
-            i7.style.cssText = `border-bottom:rgb(79, 148, 65) solid 4px; width:${pixelValues.e7}px`;
+            i7.style.cssText = `background-color: #52B788; width:${pixelValues.e7}px`;
           }, 800);
         }
         break;
@@ -92,7 +90,7 @@ line.forEach((e) => {
         let i8 = e.querySelector(".indicator");
         if (i8) {
           setTimeout(() => {
-            i8.style.cssText = `border-bottom: #00BCFF solid 4px; width:${pixelValues.e8}px`;
+            i8.style.cssText = `background-color: #00B4D8; width:${pixelValues.e8}px`;
           }, 800);
         }
         break;
@@ -100,7 +98,7 @@ line.forEach((e) => {
         let i9 = e.querySelector(".indicator");
         if (i9) {
           setTimeout(() => {
-            i9.style.cssText = `border-bottom:rgb(53, 96, 129) solid 4px; width:${pixelValues.e9}px`;
+            i9.style.cssText = `background-color:rgb(67, 145, 190); width:${pixelValues.e9}px`;
           }, 800);
         }
         break;
@@ -108,7 +106,7 @@ line.forEach((e) => {
           let i10 = e.querySelector(".indicator");
           if (i10) {
             setTimeout(() => {
-              i10.style.cssText = `border-bottom: #00684A solid 4px; width:${pixelValues.e10}px`;
+              i10.style.cssText = `background-color: #007F5F; width:${pixelValues.e10}px`;
             }, 800);
           }
           break;
@@ -132,5 +130,33 @@ line.forEach((e) => {
     setTimeout(() => {
       e.style.cssText = "opacity:1"
     }, 800);
+  }); 
+ 
+ 
+  
+  document.addEventListener('DOMContentLoaded', function() {
+
+    // Animação para os cards
+    const cards = document.querySelectorAll('.card-experience, .card-courses');
+    cards.forEach((card, index) => {
+      card.style.opacity = '0';
+      card.style.transform = 'translateY(20px)';
+      
+      setTimeout(() => {
+        card.style.opacity = '1';
+        card.style.transform = 'translateY(0)';
+      }, 300 + (index * 100));
+    });
+    
+    // Animação para os projetos
+    const projects = document.querySelectorAll('.project-card');
+    projects.forEach((project, index) => {
+      project.style.opacity = '0';
+      
+      setTimeout(() => {
+        project.style.opacity = '1';
+      }, 600 + (index * 150));
+    });
+
   });
-//  alert('Os icones em "Contatos" contém Links')
+
